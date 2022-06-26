a6ef5e74318b66d71280cabc07306c4d57beed52
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2022-06-26 14:08:13 -0400

    Add images and image handler

---
+++ ItemsDb.ts
@@ -0,0 +1,508 @@
+export const ItemsDb: any[] = [
+  {
+    title: `(TODO) Balcão Buffet Café`,
+    desc: `Balcão Buffet Café 2 Gavetas E 2 Portas Retrô 90 Olivar
+    
+    blabalbla
+    
+    
+    blebleble
+    
+    bliblibli
+    
+    
+    
+    blobloblo`,
+    id: `armariocafe`,
+    price: 300,
+    images: 5,
+  },
+  {
+    title: `(TODO) Mesa e Cadeiras da Sala`,
+    desc: `Mesa de sala marrom com 4 cadeiras`,
+    id: `mesaecadeiras`,
+    price: 300,
+    images: 6,
+  },
+  {
+    title: `(TODO) Mesa retractil de parede`,
+    desc: `Mesa retractil para parede. 4 furos`,
+    id: `mesaretratil`,
+    price: 300,
+    images: 4,
+  },
+  {
+    title: `(TODO) Microondas`,
+    desc: `Microondas Consul **CHECA litragem e modelo!**`,
+    id: `microondas`,
+    price: 300,
+    images: 4,
+  },
+  {
+    title: `(TODO) Fogão`,
+    desc: `Fogão Muller **CHECA litragem e modelo!**`,
+    id: `fogao`,
+    price: 300,
+    images: 4,
+  },
+  {
+    title: `(TODO) Alexa`,
+    desc: `Alexa **Buscar modelo**`,
+    id: `alexa`,
+    price: 300,
+    images: 3,
+  },
+  {
+    title: `(TODO) Mesa de cabeceira`,
+    desc: `Mesa de cabeceira personalizada (2 unidades)`,
+    id: `mesadecabeceira`,
+    price: 300,
+    images: 5,
+  },
+  {
+    title: `Air Fryer 3.2 L`,
+    desc: `Air Fryer MAXIS 3.2L
+      2 anos de uso
+      
+      Referência:
+      https://www.buscape.com.br/fritadeira/fritadeira-eletrica-sem-oleo-maxis-air-fryer-af-33-capacidade-3-2l`,
+    id: `airfryer`,
+    price: 250,
+    images: 4,
+  },
+
+  {
+    title: `Filtro de agua Consul`,
+    desc: `Filtro de água Gelada/Natural consul
+    aproximadamente 1 ano de uso
+    
+    Sem detalhes. Filtro trocado em Maio/2022
+    
+    Referência:
+    https://www.amazon.com.br/gp/product/B078S8GX9Q/ref=ppx_yo_dt_b_asin_title_o08_s00?ie=UTF8&th=1`,
+    id: `filtrodeagua`,
+    price: 700,
+    images: 3,
+  },
+  {
+    title: `Sofá 4 lugares`,
+    desc: `Sofá 4 lugares sob incomenda retratil + duas almofadas de costas na mesma cor
+    
+    O forro de baixo está rasgado, porém não é visível`,
+    id: `sofa`,
+    price: 1400,
+    images: 4,
+  },
+  {
+    title: `TV Sony Bravia 48 polegadas`,
+    desc: `Modelo 48W655D
+    
+    Aproximadamente 3 anos de uso. Sem detalhes
+    
+    https://www.sony.com.br/electronics/support/televisions-projectors-lcd-tvs/kdl-48w655d/specifications`,
+    id: `tv`,
+    price: 1000,
+    images: 3,
+  },
+  {
+    title: `Cadeira Gamer (2 unidades)`,
+    desc: `Cadeira Gamer Mad Racer V8 Turbo PRETA
+
+    Duas unidades disponíveis
+
+    Aproximadamente 2 anos de uso.
+    
+    Poucas marcas de uso`,
+    id: `cadeiragamer`,
+    price: 1100,
+    images: 5,
+  },
+  {
+    title: `(TODO) Armário de cozinha`,
+    desc: `Armário de cozinha branco Buscar medidas`,
+    id: `armariocozinha`,
+    price: 300,
+    images: 3,
+  },
+  {
+    title: `(TODO) Catefeira (Buscar marca e modelo)`,
+    desc: `Cafeteira blablabla tempo de uso e etc`,
+    id: `cafeteira`,
+    price: 300,
+    images: 5,
+  },
+  {
+    title: `Geladeira BRM45 HKBNA EVOX`,
+    desc: `Geladeira Duplex de 375L. Descascando na parte inferior da porta, conforme imagens
+
+    Aproximadamente 1 Ano de uso
+    
+    Referência:
+    https://www.magazineluiza.com.br/geladeira-refrigerador-brastemp-frost-free-evox-duplex-375-litros-brm45-hkbna/p/218891700/ed/refr/`,
+    id: `geladeira`,
+    price: 2000,
+    images: 8,
+  },
+  {
+    title: `(TODO) Mesa de varanda`,
+    desc: `Mesa de varanta sem cadeiras`,
+    id: `mesavaranda`,
+    price: 300,
+    images: 4,
+  },
+  {
+    title: `Weasy para cachorros de pequeno porte`,
+    desc: `Banheiro para cachorros. Direto para o ralo. Com parede para cachorros machos
+
+    https://www.weasy.com.br/products/weasy-cao-banheiro-para-cachorro?variant=39760912908367
+    `,
+    id: `weasy`,
+    price: 300,
+    images: 4,
+  },
+  {
+    title: `(TODO) Escrivanina Modelo 1`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Escrivanina Modelo 2`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Torradeira`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Processador`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Móvel area de serviço`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Prateleiras`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Escada`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Lixeiras cromadas pequena`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Lixeira cromada grande`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Organizadores pequenos`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Tapete gelado de cachorro (NOVO)`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Aspirador de pó`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Cama`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Máquina de lavar`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Bota Ortopédica`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Muletas`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Óculos Rift`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Desktop`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Joystick X52`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Monitor Ultrawide LG`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Suporte monitor articulado Elgr`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Varal de teto`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Organizadores modelo 2`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Mop vertical`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Cestos de roupa suja`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Organizador 3`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Suportes cromados`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Porta temperos`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Jarros decorativos`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Suporte para cápsulas + caixinha para cápsulas`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Placa decorativa`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Garrafa de Café`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Kit coqueteleira`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Kit copos`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Organizador 3`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Caixa de ferramentas`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Violão`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) MOchila anti-furto`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Mini-retifica`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Parafusadeira/furadeira elétrica`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Ventilador`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Lampadas smart`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Joystick Sidewinder ForceFeedback 2`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Corda de pular`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Luva Box`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Escova secadora`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+
+  {
+    title: `(TODO) Babyliss`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+
+  {
+    title: `(TODO) Chapinha`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+  {
+    title: `(TODO) Organizadores de maquiagem acrílico`,
+    desc: ``,
+    id: ``,
+    price: 0,
+    images: 0,
+  },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,72 +1,330 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {
+  Button,
+  Container,
+  Grid,
+  Link,
+  Paper,
+  Stack,
+  styled,
+  Typography,
+} from "@mui/material";
+
+import Carousel from "react-material-ui-carousel";
+
+import Modal from "@mui/material/Modal";
+import { Box, keyframes } from "@mui/system";
+import GithubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+
+import type { NextPage } from "next";
+import Image from "next/image";
+import { FC, useState } from "react";
+import { ItemsDb } from "../ItemsDb";
+
+const HeaderSection = styled("section")({
+  position: "relative",
+  height: "100vh",
+});
+
+const StyledHeader = styled("header")({
+  zIndex: "2",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "end",
+  height: "100%",
+});
+
+const BodySection = styled("section")({
+  zIndex: 2,
+  position: "relative",
+
+  backgroundImage:
+    "linear-gradient(to right top, #ffdf00, #ebe300, #d7e600, #c0e900, #a8eb12)",
+  borderTop: "14px solid black",
+  minHeight: "420px",
+  paddingTop: "120px",
+  paddingBottom: "64px",
+});
+
+const FooterSection = styled("section")({
+  position: "relative",
+  padding: "22px 0",
+  minHeight: "190px",
+  backgroundColor: "black",
+  color: "white",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "end",
+  alignItems: "center",
+});
+
+const ImageWrapper = styled("div")({
+  position: "fixed",
+  left: 0,
+  right: 0,
+  bottom: 0,
+  top: 0,
+});
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  backgroundColor: "black",
+}));
+
+const upAndDown = keyframes`
+  from {
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY(-18px);
+  }
+`;
+
+const DownIndicator = styled(KeyboardDoubleArrowDownIcon)({
+  fontSize: "80px",
+  animation: `${upAndDown} 0.5s infinite alternate`,
+});
+
+const ImageModal: FC<any> = ({
+  open,
+  onClose = () => undefined,
+}: {
+  open: boolean;
+  onClose: () => any;
+}) => (
+  <Modal
+    open={open}
+    onClose={onClose}
+    aria-labelledby="modal-modal-title"
+    aria-describedby="modal-modal-description"
+  >
+    <Box
+      sx={{
+        position: "absolute" as "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 400,
+        bgcolor: "background.paper",
+        border: "2px solid #000",
+        boxShadow: 24,
+        p: 4,
+      }}
+    >
+      <Typography id="modal-modal-title" variant="h6" component="h2">
+        Text in a modal
+      </Typography>
+      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+      </Typography>
+    </Box>
+  </Modal>
+);
+
+const Item: FC<any> = ({ item }) => {
+  return (
+    <StyledPaper>
+      <Box mb={2}>
+        <Carousel
+          autoPlay={false}
+          animation={"slide"}
+          navButtonsAlwaysVisible
+          cycleNavigation={false}
+        >
+          {Array(item.images)
+            .fill(null)
+            .map((img, index) => (
+              <div style={{ height: "500px" }}>
+                <Image
+                  width="100%"
+                  height="100%"
+                  objectFit="cover"
+                  layout="fill"
+                  src={`/${item.id}/${index + 1}.jpeg`}
+                />
+              </div>
+            ))}
+        </Carousel>
+      </Box>
+      <Box p={3}>
+        <Typography variant="h5" color="rgb(180 234 9)">
+          {item.title}
+        </Typography>
+        <Box mt={1}>
+          <Typography
+            variant="body1"
+            sx={{ whiteSpace: "pre-line" }}
+            color="rgb(246 255 216)"
+          >
+            {item.desc}
+          </Typography>
+        </Box>
+        <Box mt={2}>
+          <Stack>
+            <Stack direction={"row"} spacing={1} alignItems="center">
+              <Typography variant="body1">
+                <b>Tempo de uso:</b>
+              </Typography>
+              <Typography variant="body1">1 ano</Typography>
+            </Stack>
+          </Stack>
+        </Box>
+        <Button
+          variant="contained"
+          startIcon={<WhatsAppIcon />}
+          color="success"
+        >
+          Entrar em contato
+        </Button>
+        <Box mt={4}>
+          <Typography component="span" variant="h5" color="success.light">
+            R$ {item.price}
+          </Typography>
+        </Box>
+      </Box>
+    </StyledPaper>
+  );
+};
+
+const Body: FC = () => {
+  return (
+    <Grid container spacing={10}>
+      {ItemsDb.map((item, i) => (
+        <Grid key={i} item sm={12} md={6} lg={4}>
+          <Item item={item} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
 
 const Home: NextPage = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+    <div
+      style={{
+        backgroundImage:
+          "linear-gradient(to right top, #ffdf00, #ebe300, #d7e600, #c0e900, #a8eb12)",
+      }}
+    >
+      <HeaderSection aria-label="header-section">
+        <ImageWrapper>
+          <Image
+            width="100%"
+            height="100%"
+            objectFit="cover"
+            objectPosition="0% 25%"
+            layout="fill"
+            style={{ filter: "brightness(0.5)" }}
+            src="/borbaejosi.png"
+          />
+          <Image
+            width="100%"
+            height="100%"
+            objectFit="revert"
+            layout="fill"
+            style={{ mixBlendMode: "multiply", opacity: "20%" }}
+            src="/carbon.png"
+          />
+          <div
+            style={{
+              backgroundImage:
+                "linear-gradient(to right top, #ffdf00, #ebe300, #d7e600, #c0e900, #a8eb12)",
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              opacity: "50%",
+              mixBlendMode: "darken",
+            }}
+          ></div>
+        </ImageWrapper>
+        <StyledHeader aria-label="header">
+          <Box
+            pb={6}
+            zIndex={2}
+            display="flex"
+            flexDirection={"column"}
+            justifyContent={"center"}
+            textAlign="center"
+            color="white"
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <Typography variant="h1" fontWeight="bold">
+              Josi e Borba
+            </Typography>
+            <Box pt={3}>
+              <Typography variant="h3">Catálogo de vendas</Typography>
+            </Box>
+            <Box pt={4} display={"flex"} justifyContent="center">
+              <DownIndicator />
+            </Box>
+          </Box>
+        </StyledHeader>
+      </HeaderSection>
+      <BodySection aria-label="body-section">
+        {/* <Button onClick={() => setModalOpen(true)}>Open Modal</Button> */}
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+        <Box pt={6} />
+        <Container maxWidth="xl">
+          <Body />
+        </Container>
+      </BodySection>
+      <FooterSection>
+        <Stack direction={"row"} spacing={2}>
+          <Link
+            underline="none"
+            color="white"
+            href="https://github.com/rjborba"
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+            <Stack direction={"row"} spacing={0.5}>
+              <GithubIcon />
+              <Typography>rjborba</Typography>
+            </Stack>
+          </Link>
+          <Link
+            underline="none"
+            color="white"
+            href="https://twitter.com/rjborba"
+          >
+            <Stack direction={"row"} spacing={0.5}>
+              <TwitterIcon />
+              <Typography>rjborba</Typography>
+            </Stack>
+          </Link>
+          <Link
+            underline="none"
+            color="white"
+            href="https://www.instagram.com/rjborba/"
+          >
+            <Stack direction={"row"} spacing={0.5}>
+              <InstagramIcon />
+              <Typography>rjborba</Typography>
+            </Stack>
+          </Link>
+          <Link
+            underline="none"
+            color="white"
+            href="https://www.facebook.com/rjborbalive"
+          >
+            <Stack direction={"row"} spacing={0.5}>
+              <FacebookIcon />
+              <Typography>rjborbalive</Typography>
+            </Stack>
+          </Link>
+        </Stack>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        <Box pt={3}>
+          <Typography>© 2022</Typography>
+        </Box>
+      </FooterSection>
+      <ImageModal open={isModalOpen} onClose={() => setModalOpen(false)} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
